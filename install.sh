d9b490b9eed61ac5f25dcad552a9573c71ef77b5
Niranjan Godbole <niranjan8192@gmail.com>
2018-03-08 15:17:17 +0000

    adds basic packages

--- install.sh
+++ install.sh
@@ -16,6 +16,27 @@ cd /u16
 echo "================= Updating package lists ==================="
 apt-get update
 
+apt-get install -y -q \
+  build-essential=12.1ubuntu2* \
+  curl=7.47.0* \
+  gcc=4:5.3.1* \
+  gettext=0.19.7* \
+  htop=2.0.1* \
+  libxml2-dev=2.9.3+dfsg1* \
+  libxslt1-dev=1.1.28* \
+  make=4.1* \
+  nano=2.5.3* \
+  openssh-client=1:7.2p2* \
+  openssl=1.0.2g* \
+  software-properties-common=0.96.20.7* \
+  sudo=1.8.16* \
+  texinfo=6.1.0.dfsg.1* \
+  unzip=6.0* \
+  wget=1.17.1* \
+  rsync=3.1.1* \
+  psmisc=22.21* \
+  vim=2:7.4.1689*
+
 echo "================= Adding some global settings ==================="
 mv gbl_env.sh /etc/profile.d/
 mkdir -p "$HOME/.ssh/"
